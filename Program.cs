7027667a5e065768199f2f5fe2351477071e6236
Askorbinochka <tetyana.maletska@plp-lp.com.ua>
2026-09-10 12:33:49 +0300

    create class Client

--- Program.cs
+++ Program.cs
@@ -1,6 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 
+class Client
+{
+    public string Name { get; set; }
+
+    public Client(string name)
+    {
+        Name = name;
+    }
+
+    public void GetMessage(string text)
+    {
+        Console.WriteLine(Name + " отримав(ла) SMS: " + text);
+    }
+}
+
 class Program
 {
     static void Main()
